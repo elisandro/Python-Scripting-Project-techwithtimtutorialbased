a56859bbc535238381024f53cc079503c711c8a7
Elisandro Barajas <3114399+elisandro@users.noreply.github.com>
2025-10-14 23:11:29 -0700

    code to obtain source and target paths

--- get_game_data_mine.py
+++ get_game_data_mine.py
@@ -4,6 +4,15 @@ import shutil
 from subprocess import PIPE, run
 import sys
 
+def main(source, target):
+	cwd = os.getcwd()
+	source_path = os.path.join(cwd, source)
+	target_path = os.path.join(cwd, target)
+
 if __name__ == "__main__":
 	args = sys.argv
-	print(args)
+	if len(args) != 3:
+		raise Exception("You must pass a source and target directory - only.")
+
+	source, target = args =[1:]
+	main(source, target)
