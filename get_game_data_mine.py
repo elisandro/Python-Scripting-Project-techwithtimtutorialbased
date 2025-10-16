5d997b53691ad332679e4bd7978b819c4f042f39
Elisandro Barajas <3114399+elisandro@users.noreply.github.com>
2025-10-16 15:24:39 -0700

    Clearing contents of target directory and copying files with game in the dirctory name in the data directory over with a same directory title minus the word game

--- get_game_data_mine.py
+++ get_game_data_mine.py
@@ -34,16 +34,25 @@ def create_dir(path):
 	if not os.path.exists(path):
 		os.mkdir(path)
 
+
+def copy_and_overwrite(source, dest):
+	if os.path.exists(dest):
+		shutil.rmtree(dest)
+	shutil.copytree(source, dest)
+
 def main(source, target):
 	cwd = os.getcwd()
 	source_path = os.path.join(cwd, source)
 	target_path = os.path.join(cwd, target)
 
 	game_paths = find_all_game_paths(source_path)
-	new_game_dirs = get_name_from_paths(game_paths, "game")
-	print(new_game_dirs)
-
+	new_game_dirs = get_name_from_paths(game_paths, "_game")
+	
 	create_dir(target_path)
+
+	for src, dest in zip(game_paths, new_game_dirs):
+		dest_path = os.path.join(target_path, dest)
+		copy_and_overwrite(src, dest_path)
 
 
 if __name__ == "__main__":
